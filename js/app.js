0a01dffb7df8cecc4a4a6443f7350be4296d2100
이무원 <lmwmason@naver.com>
2025-08-02 14:44:44 +0900

    Until 3.8

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-const title = document.querySelector('.hello h1');
+const title = document.querySelector('.hello button');
+
+function handleTitleClick() {
+    if (title.style.backgroundColor == 'blue') {
+        title.style.backgroundColor = 'white';
+    } else {
+        title.style.backgroundColor = 'blue';
+    }
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "lightblue";
+}
+
+function handleWindowCopy(){
+    alert('Copied!');
+}
+
+title.addEventListener('click', handleTitleClick);
+
+window.addEventListener('resize', handleWindowResize);
+
+window.addEventListener('copy', handleWindowCopy);
